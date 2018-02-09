936228df2f5bf46bf4917ed637dd25515b3d5ce8
tfpractice <jackson.brebnor@gmail.com>
2018-02-09 13:58:16 -0500

    imports faker

--- src/models/table/model.js
+++ src/models/table/model.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
+import faker from 'faker';
 
 import TableSchema from './schema';
 
+console.log('faker.random.word()', faker.random.word());
 const Table = mongoose.model('Table', TableSchema);
 
 export default Table;
