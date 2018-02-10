4e7c0dbb56318c294c9eeadc4a3e51c536367293
tfpractice <jackson.brebnor@gmail.com>
2018-02-09 19:27:12 -0500

    adds party state

--- src/models/party/schema.js
+++ src/models/party/schema.js
@@ -2,15 +2,18 @@ import Faker from 'faker';
 import bluebird from 'bluebird';
 import mongoose, { Schema } from 'mongoose';
 
+import { PARTY_STATES, PENDING } from './helpers';
+
 mongoose.Promise = bluebird;
 
 const PartySchema = new Schema(
   {
+    name: { type: String, default: Faker.name.firstName },
     size: {
       type: Number,
       default: () => Faker.random.number({ min: 2, max: 15 }),
     },
-    name: { type: String, default: Faker.name.firstName },
+    state: { type: String, enum: PARTY_STATES, default: PENDING },
   },
   {
     toObject: { getters: true, virtuals: false },
