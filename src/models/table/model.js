378a321906884c6b04eb25a5d52fb13317eb377e
tfpractice <jackson.brebnor@gmail.com>
2018-02-09 19:08:34 -0500

    seeds tables collection

--- src/models/table/model.js
+++ src/models/table/model.js
@@ -1,9 +1,35 @@
-import mongoose from 'mongoose';
 import faker from 'faker';
+import bluebird from 'bluebird';
+import mongoose from 'mongoose';
+
+mongoose.Promise = bluebird;
 
 import TableSchema from './schema';
 
-console.log('faker.random.word()', faker.random.word());
 const Table = mongoose.model('Table', TableSchema);
 
+const newTables = (count = 5) =>
+  bluebird.all([ ...Array(count).keys() ].map(x => Table.create({})));
+
+// newTables(10).then(tables => {
+//   console.log('tables', tables);
+// });
+
+// Table.create({}).then(cr => console.log('cr', cr));
+Table.find()
+  .exec()
+
+  // .map((t, i) => {
+  //   console.log('i', i);
+  //
+  //   // console.log('t,i', t, i);
+  //   return t;
+  // })
+
+  // .exec()
+  .then(tables => console.log('tables', tables));
+
+// Table.create({}).then(table => console.log('table', table));
+
+// console.log('newTables()', newTables());
 export default Table;
